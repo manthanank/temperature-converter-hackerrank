7b237371d4f894d75f85ce7f366679d4fb52d29f
Admin <admin.push@hackerrank.workspace>
2022-04-17 08:30:35 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/app/temperatureConverter/temperatureConverter.component.ts
+++ src/app/temperatureConverter/temperatureConverter.component.ts
@@ -8,9 +8,25 @@ import {Component, Input, OnInit} from '@angular/core';
 
 export class TemperatureConverter implements OnInit {
 
-  ngOnInit() {
-    // C = (F − 32) × 5/9
-    // F = C*9/5 + 32
+  c = "";
+  f = "";
+  constructor() {}
+
+  ngOnInit() {}
+
+  onChange(value: string | null, type: "c" | "f") {
+    if (value === null) {
+      this.c = "";
+      this.f = "";
+      return;
+    }
+
+    const temperature = Number(value);
+    if (type === "c") {
+      this.f = ((temperature * 9) / 5 + 32).toFixed(1);
+    } else {
+      this.c = (((temperature - 32) * 5) / 9).toFixed(1);
+    }
   }
 
 }
